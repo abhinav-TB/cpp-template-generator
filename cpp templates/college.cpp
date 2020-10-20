a09c48355eb1dbf828a920f8e768d23d5b6a6fc5
abhinav-TB <abhinavtb@gmail.com>
2020-10-20 21:52:20 +0530

    first commit

---
+++ cpp templates/college.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+# include <math.h>
+
+using namespace std;
+
+class Any
+{
+private:
+    /* data */
+public:
+    Any(/* args */);
+    ~Any();
+};
+
+Any::Any(/* args */)
+{
+}
+
+Any::~Any()
+{
+}
+
+int main(){
+    
+    return 0;
+}
---
+++ src/index.js
@@ -0,0 +1,31 @@
+var inquirer = require('inquirer');
+const fs=require('fs');
+
+
+inquirer
+  .prompt([
+    {
+      type:'input',message:"cpp_file name",name:"name"
+  },
+    {
+        type:'list',message:"choose a template",name:"template",choices:['basic','college','cp']
+    }
+  ])
+  .then(answers => {
+    
+
+    const cwd =process.cwd();
+    // writeFileSync(path+'/template.cpp',template);
+    fs.readFileSync(cwd+`/cpp templates/${answers.template}.cpp`, "utf8");
+    fs.copyFile(cwd+`/cpp templates/${answers.template}.cpp`,cwd+`/${answers.name}.cpp`,(err)=>{
+        console.log(err);
+    })
+    console.log("yout template is ready 🎉")
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      console.log(error)
+    } else {
+      console.log(error);
+    }
+  });

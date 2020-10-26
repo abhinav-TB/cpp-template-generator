067fbf6ef0615c4d0f1d51f64adf9f349b778576
abhinav-TB <abhinavtb@gmail.com>
2020-10-26 21:46:01 +0530

    new-templates

---
+++ script.sh
@@ -0,0 +1,15 @@
+# # ! /bin/bash
+
+# # echo command
+# echo Hello world
+
+# # variables
+# # upper case by convention
+
+# NAME="Abhinav"
+# echo "my name is ${NAME}"
+
+# USER INPUT
+# read -p "enter your name: " NAME
+echo "hello $1 nice to meet you !"
+

40a0672e6e2a2855cd2899e76bd4cf54263fe5d2
abhinav-TB <abhinavtb@gmail.com>
2021-02-07 14:03:13 +0530

    revert

---
+++ dest/index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer = require('inquirer');
+const fs = require('fs');
+const user = process.env.USER;
+var args = process.argv;
+var name = args[2];
+let filenames = fs.readdirSync(`/home/${user}/cpp_templates`);
+// filenames.forEach(fi=>{
+//   console.log(fiee);
+// })
+inquirer
+    .prompt([
+    {
+        type: 'list', message: "choose a template", name: "template", choices: filenames
+    }
+])
+    .then((answers) => {
+    const cwd = process.cwd();
+    // writeFileSync(path+'/template.cpp',template);
+    fs.readFileSync(`/home/${user}/cpp_templates/${answers.template}`, "utf8");
+    fs.copyFile(`/home/${user}/cpp_templates/${answers.template}`, cwd + `/${name}.cpp`, (err) => {
+        // console.log(err);
+    });
+    console.log("your template is ready 🎉");
+})
+    .catch((error) => {
+    console.log(error);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
 export {};
 var inquirer = require('inquirer');
 const fs=require('fs');
